d03f8d5d7b0c554f8fb8a1223e0ea7dc5ee85faa
ezzelden sherif <115817063+ezzsherif@users.noreply.github.com>
2023-11-01 00:05:12 +0200

    first commit
    React app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Sidebar from './containers/sidebar/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+   
+   <Sidebar/>
+   
+   
+
+    </div>
+  );
+}
+
+export default App;
